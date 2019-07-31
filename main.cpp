dafcf473d93cad0773d86bd26eb0ddcd5a064f53
gyok <gyok@ya.ru>
2019-08-01 02:16:17 +0300

    Parallel function running on vector elements
    This program compares durations of parallel and inline
    computations

--- main.cpp
+++ main.cpp
@@ -1,12 +1,65 @@
 #include "parallelFuncSum.h"
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <chrono>
 
-double duplicate(double i)
+double Duplicate(double i)
 {
   return i;
 }
 
+
+void FillVector(std::vector<double>& v)
+{
+  std::random_device rd{};
+  std::mt19937 mersenne_engine{rd()};
+  std::normal_distribution<> normal_distribution{5., 2.};
+  auto gen = [&mersenne_engine, &normal_distribution]()
+  {
+      return normal_distribution(mersenne_engine);
+  };
+
+  std::generate(v.begin(), v.end(), gen);
+}
+
+
 int main()
 {
-  const size_t vecSize = 13007;
+  const size_t vecSize = 16'000'000;
   std::vector<double> v(vecSize);
+  FillVector(v);
+  // std::cout << "Generated vector:" << std::endl;
+  // for (auto elem : v) std::cout << elem << ", ";
+  // std::cout << std:: endl << std:: endl;
+
+  std::function<double(double)> Func = &Duplicate;
+
+
+  auto start = std::chrono::high_resolution_clock().now();
+
+  auto sum = func_sum(v.begin(), v.end(), Func);
+
+  auto finish = std::chrono::high_resolution_clock().now();
+  auto duration = static_cast<double>(std::chrono::duration_cast<std::chrono::microseconds>(finish - start).count());
+
+
+  start = std::chrono::high_resolution_clock().now();
+
+  auto sum_parallel = parallel_func_sum(v.begin(), v.end(), Func);
+
+  finish = std::chrono::high_resolution_clock().now();
+  auto duration_parallel = static_cast<double>(std::chrono::duration_cast<std::chrono::microseconds>(finish - start).count());
+
+
+  std::cout << sum << "\t"
+            << sum_parallel
+            << (sum == sum_parallel ? " (equal)" : " (not equal)") << std::endl
+            << duration << "\t"
+            << duration_parallel << " "
+            << "(" << duration_parallel / duration << ")" << std::endl;
+
+  std::cin.get();
+
+  return 0;
 }
--- parallelFuncSum.h
+++ parallelFuncSum.h
@@ -1,47 +1,61 @@
 #pragma once
+
 #include <functional>
 #include <vector>
 #include <thread>
+#include <iostream>
+
 
 template<typename TIt, typename T>
-T parallel_func_sum(TIt i_begin, TIt i_end, std::function<T(T)>& i_func)
+T func_sum(TIt i_begin, TIt i_end, std::function<T(T)>& i_func)
 {
-  int values_count = i_end - i_begin;
-  int thread_count = std::thread::hardware_concurrency();
-  std::vector<T> thread_sum(thread_count, 0);
-  std::vector<std::thread> threads;
-  int values_for_thread_count = values_count / thread_count;
-
-  for (int i = 0; i < thread_count - 1; i++)
-  {
-    threads.emplace_back(std::thread(
-    [&i_begin,i, &i_func, &thread_sum, &values_for_thread_count]
-    {
-      int step = i * values_for_thread_count;
-      thread_sum[i] = func_sum(i_begin + step, i_begin + step + 1, i_func);
-    });
-  }
-  threads.emplace_back(std::thread(
-    [&i_begin, &i_end, &thread_count, &i_func, &thread_sum, &values_for_thread_count]
-  {
-    int step = (thread_count - 1) * values_for_thread_count;
-    thread_sum[i] = func_sum(i_begin + step, i_end, i_func);
-  });
   T sum{};
-  for (int i = 0; i < thread_count; i++)
+  std::cout << *i_begin << " -> " << *i_end << std::endl;
+  for (TIt it = i_begin; it != i_end; it++)
   {
-    sum += thread_sum[i];
+    sum += i_func(*it);
   }
   return sum;
 }
 
+
 template<typename TIt, typename T>
-T func_sum(TIt& i_begin, TIt& i_end, std::function<T(T)>& i_func)
+T parallel_func_sum(TIt i_begin, TIt i_end, std::function<T(T)>& i_func)
 {
-  T sum{};
-  for (TIt it = i_begin; it != i_end; it++)
+  size_t values_count = i_end - i_begin;
+  size_t thread_count = std::min(static_cast<size_t>(std::thread::hardware_concurrency()), values_count);
+  std::vector<T> thread_sum(thread_count, 0);
+  std::vector<std::thread> threads;
+  size_t values_for_thread_count = values_count / thread_count;
+  std::cout << "Threads count: " << thread_count << std::endl;
+
+  // running threads
+  for (size_t i = 0; i < thread_count - 1; i++)
   {
-    sum += i_func(*i_begin);
+    threads.emplace_back(
+    [&i_begin, i, &i_func, &thread_sum, &values_for_thread_count]
+    {
+      size_t step = i * values_for_thread_count;
+      thread_sum[i] = func_sum(i_begin + step, i_begin + step + values_for_thread_count, i_func);
+    });
   }
+  threads.emplace_back(
+    [&i_begin, &i_end, &thread_count, &i_func, &thread_sum, &values_for_thread_count]
+  {
+    size_t step = (thread_count - 1) * values_for_thread_count;
+    thread_sum[thread_count - 1] = func_sum(i_begin + step, i_end, i_func);
+  });
+
+  // storing result of computation
+  T sum{};
+  for (size_t i = 0; i < thread_count; i++)
+  {
+      threads[i].join();
+  }
+  for (size_t i = 0; i < thread_count; i++)
+  {
+    sum += thread_sum[i];
+  }
+
   return sum;
 }
